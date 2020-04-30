a8882a7c9d1302c8beabdad72d82b9a157b19943
jasimuddin123 <jasimuddindxn@gmail.com>
2020-04-30 06:14:58 -0700

    Make Some Dynamic data

--- src/App.js
+++ src/App.js
@@ -4,27 +4,33 @@ import './App.css';
 
 function App() {
 
+  const nayoks =['SochNagar', 'JakiSun', 'Arnald','Amir Khan'];
+  const nayikas =['kucbanu', 'chuckhatun', 'safali tak'];
+
 
   return (
     <div className="App">
       <header className="App-header">
 
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Person name="Rubel" nayika="Mousumi"></Person>
+      <Person name="Bapparaz" nayika="Shabana"></Person>
+      <Person name="Jasim Uddin" nayika="Saima"></Person>
+      <Person name={nayoks[0]} nayika={nayikas[2]}></Person>
       </header>
     </div>
   );
 }
-function Person(){
+function Person(props){
   const personStyle={
     border:'2px solid red',
     margin:'10px'
   }
+  console.log(props);
   return(
   <div style={personStyle}>
-    <h2>Name : Jasim Uddin </h2>
-    <p>Job : SP Supervisor</p>
+    <h2>Nayak:{props.name} </h2>
+  <p>Nayaka :{props.nayika}</p>
+  
 
   </div>
   )
